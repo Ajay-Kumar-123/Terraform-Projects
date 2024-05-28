2afd61e68ef5d1a58d1a5aebd71fb4deda52f6ea
Ajay Kumar <ajay@chipsy.in>
2024-05-28 11:05:48 +0530

    Created Remote Backend Terraform project

---
+++ Remote-Backend/main.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "remote-s3" {
+  bucket = "remote-backend-infra"
+}
+
+resource "aws_dynamodb_table" "state_lock-db" {
+  name = "tf-state-db"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = LockID
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
