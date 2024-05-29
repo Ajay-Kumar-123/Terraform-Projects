defa35886427e8405193bc4c401adaedcdf4fd57
Ubuntu <ubuntu@ip-172-31-43-63.ap-south-1.compute.internal>
2024-05-29 10:41:47 +0000

    Fixed indentation in Terraform-Modules-Demo

--- Terraform-Modules-Demo/main.tf
+++ Terraform-Modules-Demo/main.tf
@@ -1,6 +1,6 @@
 module "ec2_instance" {
-  source = "./modules/ec2"
-  ami_value = "ami-05e00961530ae1b55"
+  source        = "./modules/ec2"
+  ami_value     = "ami-05e00961530ae1b55"
   instance-type = "t2.micro"
-  secret = "aws-secret"
+  secret        = "aws-secret"
 }
--- Terraform-Modules-Demo/modules/ec2/main.tf
+++ Terraform-Modules-Demo/modules/ec2/main.tf
@@ -3,30 +3,30 @@ data "aws_vpc" "vpc" {
 }
 
 resource "aws_security_group" "sg" {
-  name = "New-SG"
+  name   = "New-SG"
   vpc_id = data.aws_vpc.vpc.id
 
   ingress {
     description = "SSH Port"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "HTTP Port"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     description = "All Outbound Ports"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -36,10 +36,10 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_instance" "ec2" {
-  ami = var.ami_value
-  instance_type = var.instance-type
+  ami                    = var.ami_value
+  instance_type          = var.instance-type
   vpc_security_group_ids = [aws_security_group.sg.id]
-  key_name = var.secret
+  key_name               = var.secret
 
   tags = {
     Name = "New-EC2"
