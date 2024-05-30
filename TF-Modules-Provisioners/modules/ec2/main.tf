9e3dce99bf6f7132752e4c25ceada1dd5ae834cb
Ajay Kumar <ajay@chipsy.in>
2024-05-30 13:28:08 +0530

    Error fixes in TF-Modules-Provisioners

--- TF-Modules-Provisioners/modules/ec2/main.tf
+++ TF-Modules-Provisioners/modules/ec2/main.tf
@@ -1,81 +1,81 @@
 resource "aws_security_group" "sg" {
-  name   = "SG"
-  vpc_id = aws_vpc.vpc-id
+  name = "SG"
+  vpc_id = var.vpc-id
 
   ingress {
     description = "SSH Port"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "HTTP Port"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     description = "All outbound ports"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_security_group" "lb-sg" {
-  name   = "LB-SG"
-  vpc_id = aws_vpc.vpc-id
+  name = "LB-SG"
+  vpc_id = var.vpc-id
 
   ingress {
     description = "SSH Port"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "HTTP Port"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     description = "All outbound ports"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "ec2-a" {
-  ami                    = var.ami-value
-  instance_type          = var.instance
-  subnet_id              = aws_subnet.subnet-1.id
+  ami = var.ami-value
+  instance_type = var.instance
+  subnet_id = var.subnet-1
   vpc_security_group_ids = [aws_security_group.sg.id]
-  key_name               = var.secret
+  key_name = var.secret
 
   root_block_device {
     volume_size = 10
   }
 
   connection {
-    type        = "ssh"
+    type = "ssh"
     private_key = file("~/.ssh/id_rsa")
-    user        = "ubuntu"
-    host        = self.public_ip
+    user = "ubuntu"
+    host = self.public_ip
   }
 
   provisioner "remote-exec" {
-    inline = [
+    inline = [ 
       "sudo apt update",
       "sudo apt upgrade -y",
       "sudo apt dist-upgrade -y",
@@ -83,11 +83,11 @@ resource "aws_instance" "ec2-a" {
       "sudo systemctl reload nginx",
       "sudo systemctl start nginx",
       "sudo systemctl enable nginx",
-    ]
+     ]
   }
 
   provisioner "file" {
-    source      = "/home/ubuntu/index.html"
+    source = "/home/ubuntu/index.html"
     destination = "/var/www/html"
   }
 
@@ -96,7 +96,7 @@ resource "aws_instance" "ec2-a" {
   }
 
   provisioner "local-exec" {
-    when    = destroy
+    when = destroy
     command = "echo 'EC2 instance EC2-A destroyed successfully >> destroy-message.txt"
   }
 
@@ -106,38 +106,38 @@ resource "aws_instance" "ec2-a" {
 }
 
 resource "aws_instance" "ec2-b" {
-  ami                    = var.ami-value
-  instance_type          = var.instance
-  subnet_id              = aws_subnet.subnet-2.id
+  ami = var.ami-value
+  instance_type = var.instance
+  subnet_id = aws_subnet.subnet-2.id
   vpc_security_group_ids = [aws_security_group.sg.id]
-  key_name               = var.secret
+  key_name = var.secret
 
   root_block_device {
     volume_size = 10
   }
 
   connection {
-    type        = "ssh"
+    type = "ssh"
     private_key = file("~/.ssh/id_rsa")
-    user        = "ubuntu"
-    host        = self.public_ip
+    user = "ubuntu"
+    host = self.public_ip
   }
 
 
   provisioner "remote-exec" {
-    inline = [
+    inline = [ 
       "sudo apt update",
       "sudo apt upgrade -y",
       "sudo apt dist-upgrade -y",
       "sudo apt-get install nginx -y",
       "sudo systemctl relaod nginx",
       "sudo systemctl start nginx",
-      "sudo systemctl enable nginx",
-    ]
+      "sudo systemctl enable nginx",    
+     ]
   }
 
-  provisioner "file" {
-    source      = "/home/ubuntu/index.html"
+   provisioner "file" {
+    source = "/home/ubuntu/index.html"
     destination = "/var/www/html"
   }
 
@@ -146,7 +146,7 @@ resource "aws_instance" "ec2-b" {
   }
 
   provisioner "local-exec" {
-    when    = destroy
+    when = destroy
     command = "echo 'EC2 instance, EC2-B destroyed successfully' >> destroy-message.txt"
   }
 
@@ -172,11 +172,11 @@ resource "aws_eip" "eip-b" {
 }
 
 resource "aws_eip_association" "ec2-a-assoc" {
-  instance_id   = aws_instance.ec2-a.id
+  instance_id = aws_instance.ec2-a.id
   allocation_id = aws_eip.eip-a.id
 }
 
 resource "aws_eip_association" "ec2-b-assoc" {
-  instance_id   = aws_instance.ec2-b.id
+  instance_id = aws_instance.ec2-b.id
   allocation_id = aws_eip.eip-b.id
 }
--- TF-Modules-Provisioners/modules/lb/main.tf
+++ TF-Modules-Provisioners/modules/lb/main.tf
@@ -2,8 +2,8 @@ resource "aws_lb" "alb" {
   name               = "ALB"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [aws_subnet.subnet-1.id, aws_subnet.subnet-2.id]
+  security_groups = [var.lb_sg]
+  subnets = [var.subnet-1, var.subnet-2]
 
   tags = {
     Name = "ALB"
@@ -14,7 +14,7 @@ resource "aws_lb_target_group" "alb-tg" {
   name     = "ALB-TG"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.vpc.id
+  vpc_id = var.vpc-id
 
   health_check {
     path = "/"
@@ -24,12 +24,12 @@ resource "aws_lb_target_group" "alb-tg" {
 
 resource "aws_lb_target_group_attachment" "tg-att-1" {
   target_group_arn = aws_lb_target_group.alb-tg.arn
-  target_id        = aws_instance.ec2-a.id
+  target_id = var.ec2-a
 }
 
 resource "aws_lb_target_group_attachment" "tg-att-2" {
   target_group_arn = aws_lb_target_group.alb-tg.id
-  target_id        = aws_instance.ec2-b.id
+  target_id = var.ec2-b
 }
 
 resource "aws_lb_listener" "alb-listener" {
