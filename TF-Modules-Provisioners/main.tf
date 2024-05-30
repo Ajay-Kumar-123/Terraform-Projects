5a07a7bb127058a7ed13e140dc056a5d85a0aa2d
Ubuntu <ubuntu@ip-172-31-10-69.ap-south-1.compute.internal>
2024-05-30 09:28:57 +0000

    Fixed Indentation issues in TF-Modules-Provisioners

--- TF-Modules-Provisioners/main.tf
+++ TF-Modules-Provisioners/main.tf
@@ -1,17 +1,17 @@
 module "vpc" {
-  source = "./modules/vpc"
-  vpc_cidr = "10.0.0.0/23"
+  source       = "./modules/vpc"
+  vpc_cidr     = "10.0.0.0/23"
   subnet1-cidr = "10.0.0.0/24"
   subnet2-cidr = "10.0.1.0/24"
-  subnet1-az = "ap-south-1a"
-  subnet2-az = "ap-south-1b"
+  subnet1-az   = "ap-south-1a"
+  subnet2-az   = "ap-south-1b"
 }
 
 module "ec2" {
-  source = "./modules/ec2"
+  source    = "./modules/ec2"
   ami-value = "ami-05e00961530ae1b55"
-  instance = "t2.micro"
-  secret = "aws-secret"
+  instance  = "t2.micro"
+  secret    = "aws-secret"
 }
 
 module "lb" {
--- TF-Modules-Provisioners/modules/lb/main.tf
+++ TF-Modules-Provisioners/modules/lb/main.tf
@@ -1,9 +1,9 @@
 resource "aws_lb" "alb" {
-  name = "ALB"
-  internal = false
+  name               = "ALB"
+  internal           = false
   load_balancer_type = "application"
-  security_groups = [var.lb_sg]
-  subnets = [var.subnet-1, var.subnet-2]
+  security_groups    = [var.lb_sg]
+  subnets            = [var.subnet-1, var.subnet-2]
 
   tags = {
     Name = "ALB"
@@ -11,10 +11,10 @@ resource "aws_lb" "alb" {
 }
 
 resource "aws_lb_target_group" "alb-tg" {
-  name = "ALB-TG"
-  port = 80
+  name     = "ALB-TG"
+  port     = 80
   protocol = "HTTP"
-  vpc_id = var.vpc-id
+  vpc_id   = var.vpc-id
 
   health_check {
     path = "/"
@@ -24,22 +24,22 @@ resource "aws_lb_target_group" "alb-tg" {
 
 resource "aws_lb_target_group_attachment" "tg-att-1" {
   target_group_arn = aws_lb_target_group.alb-tg.arn
-  target_id = var.ec2-a
+  target_id        = var.ec2-a
 }
 
 resource "aws_lb_target_group_attachment" "tg-att-2" {
   target_group_arn = aws_lb_target_group.alb-tg.id
-  target_id = var.ec2-b
+  target_id        = var.ec2-b
 }
 
 resource "aws_lb_listener" "alb-listener" {
   load_balancer_arn = aws_lb.alb.arn
-  port = 80
-  protocol = "HTTP"
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
     target_group_arn = aws_lb_target_group.alb-tg.arn
-    type = "forward"
+    type             = "forward"
   }
 }
 
