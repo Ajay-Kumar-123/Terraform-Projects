8a9867cfe3766558320c4fe84df71ddcff25b6f0
Ubuntu <ubuntu@ip-172-31-9-236.ap-south-1.compute.internal>
2024-05-31 07:14:50 +0000

    Corrected indentation in Count-Hard-Coding

--- Count-Hard-Coding/main.tf
+++ Count-Hard-Coding/main.tf
@@ -3,30 +3,30 @@ data "aws_vpc" "vpc" {
 }
 
 resource "aws_security_group" "sg" {
-  name = "Test-SG"
+  name   = "Test-SG"
   vpc_id = data.aws_vpc.vpc.id
 
   ingress {
     description = "SSH Port"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "HTTP Port"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     description = "All outbound ports"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -36,11 +36,11 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_instance" "ec2" {
-  ami = var.ami
-  instance_type = var.instance-type
+  ami                    = var.ami
+  instance_type          = var.instance-type
   vpc_security_group_ids = [aws_security_group.sg.id]
-  key_name = var.secret
-  count = 3
+  key_name               = var.secret
+  count                  = 3
 
   tags = {
     Name = "Server-${count.index}"
