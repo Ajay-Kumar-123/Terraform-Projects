b9c0717b3040d1ff49282f8249c62432350ddd54
Ubuntu <ubuntu@ip-172-31-15-2.ap-south-1.compute.internal>
2024-06-02 07:20:36 +0000

    Corrected indentation in Lifecycle-Prevent-Destroy

--- Lifecycle-Prevent-Destroy/main.tf
+++ Lifecycle-Prevent-Destroy/main.tf
@@ -3,30 +3,30 @@ data "aws_vpc" "vpc" {
 }
 
 resource "aws_security_group" "sg" {
-  name = "SG"
+  name   = "SG"
   vpc_id = data.aws_vpc.vpc.id
 
   ingress {
     description = "SSH-Port"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "HTTP Port"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     description = "All outbound ports"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -36,10 +36,10 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_instance" "ec2" {
-  ami = var.ami-value
-  instance_type = var.instance
+  ami                    = var.ami-value
+  instance_type          = var.instance
   vpc_security_group_ids = [aws_security_group.sg.id]
-  key_name = var.secret
+  key_name               = var.secret
 
   lifecycle {
     prevent_destroy = true
