a8b528ee2bd972afa5abbce6203369ae30d59a54
Ubuntu <ubuntu@ip-172-31-34-212.ap-south-1.compute.internal>
2024-06-02 15:29:04 +0000

    Fixed issues in TF-Workspace

--- TF-Workspace/main.tf
+++ TF-Workspace/main.tf
@@ -7,9 +7,9 @@ resource "aws_vpc" "vpc" {
 }
 
 resource "aws_subnet" "subnet" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = "0.0.0.0/24"
-  availability_zone = "ap-south-1a"
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "0.0.0.0/24"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -40,34 +40,34 @@ resource "aws_route_table" "rt" {
 
 resource "aws_route_table_association" "rt-assoc" {
   route_table_id = aws_route_table.rt.id
-  subnet_id = aws_subnet.subnet.id
+  subnet_id      = aws_subnet.subnet.id
 }
 
 resource "aws_security_group" "sg" {
-  name = "SG"
+  name   = "SG"
   vpc_id = aws_vpc.vpc.id
 
   ingress {
     description = "SSH Port"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "HTTP Port"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     description = "All outbound ports"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -77,11 +77,11 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_instance" "ec2" {
-  ami = var.ami-value
-  instance_type = var.instance
-  subnet_id = aws_subnet.subnet.id
+  ami                    = var.ami-value
+  instance_type          = var.instance
+  subnet_id              = aws_subnet.subnet.id
   vpc_security_group_ids = [aws_security_group.sg.id]
-  key_name = var.secret
+  key_name               = var.secret
 
   tags = {
     Name = terraform.workspace
