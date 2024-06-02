ae5d9d7d935e2e21d56ffda119c898cb527abc7e
Ubuntu <ubuntu@ip-172-31-15-2.ap-south-1.compute.internal>
2024-06-02 06:19:20 +0000

    Corrected indentation of files in For-Each-Map

--- For-Each-Map/main.tf
+++ For-Each-Map/main.tf
@@ -3,30 +3,30 @@ data "aws_vpc" "vpc" {
 }
 
 resource "aws_security_group" "dev-sg" {
-  name = "Dev-SG"
+  name   = "Dev-SG"
   vpc_id = data.aws_vpc.vpc.id
 
   ingress {
     description = "SSH Port"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "HTTP Port"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     description = "All outbound ports"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -36,30 +36,30 @@ resource "aws_security_group" "dev-sg" {
 }
 
 resource "aws_security_group" "staging-sg" {
-  name = "Staging-SG"
+  name   = "Staging-SG"
   vpc_id = data.aws_vpc.vpc.id
 
   ingress {
     description = "SSH Port"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "HTTP Port"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     description = "All outbound ports"
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -71,24 +71,24 @@ resource "aws_security_group" "staging-sg" {
 locals {
   instances = {
     "Dev" = {
-        ami = "ami-05e00961530ae1b55"
-        instance_type = "t2.micro"
-        security_groups = [aws_security_group.dev-sg.id]
+      ami             = "ami-05e00961530ae1b55"
+      instance_type   = "t2.micro"
+      security_groups = [aws_security_group.dev-sg.id]
     },
     "Staging" = {
-        ami = "ami-05e00961530ae1b55"
-        instance_type = "t2.small"
-        security_groups = [aws_security_group.staging-sg.id]
+      ami             = "ami-05e00961530ae1b55"
+      instance_type   = "t2.small"
+      security_groups = [aws_security_group.staging-sg.id]
     }
   }
 }
 
 resource "aws_instance" "ec2" {
-  for_each = local.instances
-  ami = each.value.ami
-  instance_type = each.value.instance_type
+  for_each               = local.instances
+  ami                    = each.value.ami
+  instance_type          = each.value.instance_type
   vpc_security_group_ids = each.value.security_groups
-  key_name = "aws-secret"
+  key_name               = "aws-secret"
 
   tags = {
     Name = each.key
